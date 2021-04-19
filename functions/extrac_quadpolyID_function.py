598bf1dbb5a5faf2f95ca90b1dd6f17e40d25bf1
Robert Ritson <81387018+r2j2ritson@users.noreply.github.com>
2021-04-19 14:40:07 -0600

    extract_quadpolyID_function.py
    Updated extract_quadpolyID_fuction, worked out some of the bugs I was running into...

--- functions/extrac_quadpolyID_function.py
+++ functions/extrac_quadpolyID_function.py
@@ -1,26 +1,31 @@
-#--------------Python Function for Extracting QuadPoly ID-----------------------#
-## Updated to include functionality for creating a file geodatabase if missing ##
-#-------------------------------------------------------------------------------#
-# EXPLANATION: Function contains nested for loops for dealing with two optional
-#              input features, 'intercept_feature' & 'gdb_name'
-#
-# 'intercept_feature' defaults to 'None'
-#       This argument takes the file name of a geodatabase (only use if field data contained in file geodatabase)
-#       If 'None', then the field data file path is assumed to be to a shapefile
-#
-# 'gdb_name' defaults to 'None'
-#       This argument takes the input '[name].gdb' and creates an empty file geodatabase with that name, then updates 'path_to_output_gdb'
-#           to include it
-#       If 'None', then 'path_to_output_gdb' assumed to be to a valid .gdb
-#
-# REQUIRED ARGUMENTS:
-# 'path_to_pts' --> file path to location of field points (if path to a gdb, provide an argument for 'intercept_feature')
-# 'path_to_quad --> file path to geodatabase containing eCognition quadpolygons
-# 'path_to_output_gdb' --> file path to a geodatabase which will contain output shapefiles;
-#                          to create an empty gdb, provide a file path and an argument for 'gdb_name'
-# 'path_to_output_folder' --> file path to a folder which will contain individual output dbfs
-# 'mergeTable_dbf' --> file name of final merged output dbf table (must include '.dbf')
-#-------------------------------------------------------------------------------#
+#--------------Python Function for Extracting QuadPoly IDs---------------------------------------------------------------------#
+#--------------Author: Robert Ritson, Research Associate (WMI)-----------------------------------------------------------------#
+#--------------Last Updated: 4/19/2021-----------------------------------------------------------------------------------------#
+#------------------------------------------------------------------------------------------------------------------------------#
+## OPTIONAL ARGUMENTS:
+### (1) 'intercept_feature'
+###         Defaults to 'None'. This argument allows for specifying a specific layer
+###         containing a target layer within a geodatabase (for field data stored in a .gdb
+###         'None' assumes 'path_to_pts' leads to a shapefile
+#####
+### (2) 'gdb_name'
+###         Defaults to 'None'. This argument names and creates a new geodatabase for storing output
+###         'None' assumes 'path_to_output_gdb' leads to a valid geodatabase
+#####
+### (3) 'sel'
+###         Defaults to "all". This argument provides a name or names of specific quadpolygons to sort through
+###         Can be a string containing a single quadpolygon name (ex. "q41117e1_100blk") or
+###         a list of polygons (ex: ["q41117e1_100blk","q41117a1_100blk","q42117e1_100blk"]
+###         "all" sorts through all quadpolygons in the geodatabase specified by 'path_to_quad'
+#------------------------------------------------------------------------------------------------------------------------------#
+## REQUIRED ARGUMENTS:
+### (1) 'path_to_pts' --> file path to location of field points (if path to a gdb, provide an argument for 'intercept_feature')
+### (2) 'path_to_quad --> file path to geodatabase containing eCognition quadpolygons
+### (3) 'path_to_output_gdb' --> file path to a geodatabase which will contain output shapefiles;
+##       to create an empty gdb, provide a file path and an argument for 'gdb_name'
+### (4) 'path_to_output_folder' --> file path to a folder which will contain individual output dbfs
+### (5) 'mergeTable_dbf' --> file name of final merged output dbf table (must include '.dbf')
+#------------------------------------------------------------------------------------------------------------------------------#
 #Import modules
 import arcpy
 import datetime
@@ -28,96 +33,113 @@ import csv
 import os
 
 #Define function
-def extract_quadpolyID(path_to_pts, path_to_quad_gdb, path_to_output_gdb,path_to_output_folder, mergeTable_dbf, intercept_feature = None,gdb_name = None):
-    #Begin Script
+def extract_quadpolyID(path_to_pts, path_to_quad_gdb, path_to_output_gdb,path_to_output_folder, mergeTable_dbf, intercept_feature = "None",gdb_name = "None",sel= "None"):
     try:
         print 'begin script on '+datetime.datetime.now().date().isoformat()+' at '+datetime.datetime.now().time().isoformat()[0:8]
+
+        #Set paths to folders and workspace#
         plotws = path_to_pts #file location of field points/plots
         polyws = path_to_quad_gdb #gdb of quadpolygons 
         tblws =  path_to_output_folder #Output location for tables
-        #Set workspace environment
         arcpy.env.overwriteOutput = True
         arcpy.env.workspace = polyws
-        if gdb_name != None:  #if gdb_name is provided, create file geodatabase for output features
-            #Create gdb for to contain output features
-            out_folder_path = path_to_output_gdb
-            out_name = gdb_name #name of gdb to create for output features
-            arcpy.CreateFileGDB_management(out_folder_path=out_folder_path,out_name=out_name)
-            outws =  path_to_output_gdb+'/'+gdb_name
-            print 'listing blank 100k quadpolygons'
-            fc_list = [fc for fc in arcpy.ListFeatureClasses()] # adjust this to limit the quads you sort through
-            # load field points
-            print 'loading target shapefile'
-            arcpy.MakeFeatureLayer_management(plotws,'pts_lyr') #Field data shapefile
-            # loop through features
-            for fc in fc_list:
-                print 'loading feature '+fc
-                arcpy.MakeFeatureLayer_management(polyws+'/'+fc, 'tmp_fc')
-                # select polygons matching field points
-                print 'selecting ecog polygons that intersect with target shapefile'
-                arcpy.SelectLayerByLocation_management('tmp_fc', "INTERSECT", 'pts_lyr','',"NEW_SELECTION") # point intercept plots
-                # see if anything was selected
-                matchcount = int(arcpy.GetCount_management('tmp_fc')[0])
-                # if so save that fc
-                if matchcount != 0:
-                    # exporting polygons
-                    print 'exporting'
-                    arcpy.CopyFeatures_management('tmp_fc', outws+'/'+fc+'_select_pts')
-                    print fc+'_select_pts exported at '+datetime.datetime.now().time().isoformat()[0:8]
-                    # spatial join
-                    arcpy.MakeFeatureLayer_management(outws+'/'+fc+'_select_pts', 'tmp_fc_sel')
-                    arcpy.SpatialJoin_analysis('tmp_fc_sel', 'pts_lyr', outws+'/'+fc+'_join_pts', "JOIN_ONE_TO_MANY")
-                    print fc+' joined with target shapefile at '+datetime.datetime.now().time().isoformat()[0:8]
-                    # export dbf
-                    arcpy.TableToTable_conversion(outws+'/'+fc+'_join_pts', tblws, fc+'_tbl_pts.dbf')
-                    print fc+' dbf file exported at '+datetime.datetime.now().time().isoformat()[0:8]
-                else:
-                    pass
 
-            # Combine Output .dbfs into one table
+        #Set and/or create an output workspace#
+        if gdb_name == "None":  
+            outws = path_to_output_gdb
+        else:
+            arcpy.CreateFileGDB_management(out_folder_path=path_to_output_gdb,out_name=gdb_name)
+            outws =  path_to_output_gdb+'/'+gdb_name
+
+        #Create QuadPolygon List (w/selection, if prompted)#
+        try:            
+            if sel == "None":
+                print 'listing blank 100k quadpolygons'
+                fc_list = [fc for fc in arcpy.ListFeatureClasses()] 
+            elif type(sel) is str:
+                print 'listing blank 100k quadpolygons'
+                fc_list = [fc for fc in arcpy.ListFeatureClasses(sel)]
+            elif type(sel) is list:
+                print 'listing blank 100k quadpolygons'
+                fc_list =[fc for fc in sel]
+            else:
+                raise ValueError("Invalid Selection")
+        except ValueError as ve:
+            print(ve)
+            print '\n\n!!!---> SCRIPT CRASHED ON '+datetime.datetime.now().date().isoformat()+' AT '+datetime.datetime.now().time().isoformat()[0:8]+' <---!!!\n'
+
+        #Execute QuadPolygonID extraction for field pts not w/in a GDB#
+        else:
+            #Code block for specific field data shapefile
+            if intercept_feature == "None":
+                # load field points
+                print 'loading target shapefile'
+                arcpy.MakeFeatureLayer_management(plotws,'pts_lyr') #Field data shapefile
+                # loop through features
+                for fc in fc_list:
+                    print 'loading feature '+fc
+                    arcpy.MakeFeatureLayer_management(polyws+'/'+fc, 'tmp_fc')
+                    # select polygons matching field points
+                    print 'selecting ecog polygons that intersect with target shapefile'
+                    arcpy.SelectLayerByLocation_management('tmp_fc', "INTERSECT", 'pts_lyr','',"NEW_SELECTION") # point intercept plots
+                    # see if anything was selected
+                    matchcount = int(arcpy.GetCount_management('tmp_fc')[0])
+                    # if so save that fc
+                    if matchcount != 0:
+                        # exporting polygons
+                        print 'exporting'
+                        arcpy.CopyFeatures_management('tmp_fc', outws+'/'+fc+'_select_pts')
+                        print fc+'_select_pts exported at '+datetime.datetime.now().time().isoformat()[0:8]
+                        # spatial join
+                        arcpy.MakeFeatureLayer_management(outws+'/'+fc+'_select_pts', 'tmp_fc_sel')
+                        arcpy.SpatialJoin_analysis('tmp_fc_sel', 'pts_lyr', outws+'/'+fc+'_join_pts', "JOIN_ONE_TO_MANY")
+                        print fc+' joined with target shapefile at '+datetime.datetime.now().time().isoformat()[0:8]
+                        # export dbf
+                        arcpy.TableToTable_conversion(outws+'/'+fc+'_join_pts', tblws, fc+'_tbl_pts.dbf')
+                        print fc+' dbf file exported at '+datetime.datetime.now().time().isoformat()[0:8]
+                    else:
+                        pass
+                    
+        #Execute QuadPolyID extraction for field pts w/in a GDB#
+            #Code block for field data located w/in a GDB  
+            else:
+                ifc = intercept_feature # name of intercept feature in target feature layer (aka if plotws of field data is a .gdb)
+                # load field points
+                print 'loading target shapefile'
+                arcpy.MakeFeatureLayer_management(plotws+'/'+ifc, 'ifc') #field point shapefile from within a .gdb
+                # loop through features
+                for fc in fc_list:
+                    print 'loading feature '+fc
+                    arcpy.MakeFeatureLayer_management(polyws+'/'+fc, 'tmp_fc')
+                    # select polygons matching field points
+                    print 'selecting ecog polygons that intersect with target shapefile'
+                    arcpy.SelectLayerByLocation_management('tmp_fc', "INTERSECT", 'ifc', '', "NEW_SELECTION") # point intercept plots
+                    # see if anything was selected
+                    matchcount = int(arcpy.GetCount_management('tmp_fc')[0])
+                    # if so save that fc
+                    if matchcount != 0:
+                        # exporting polygons
+                        print 'exporting'
+                        arcpy.CopyFeatures_management('tmp_fc', outws+'/'+fc+'_select_ifc')
+                        print fc+'_select_ifc exported at '+datetime.datetime.now().time().isoformat()[0:8]
+                        # spatial join
+                        arcpy.MakeFeatureLayer_management(outws+'/'+fc+'_select_ifc', 'tmp_fc_sel')
+                        arcpy.SpatialJoin_analysis('tmp_fc_sel', 'ifc', outws+'/'+fc+'_join_ifc', "JOIN_ONE_TO_MANY")
+                        print fc+' joined with target shapefile at '+datetime.datetime.now().time().isoformat()[0:8]
+                        # export dbf
+                        arcpy.TableToTable_conversion(outws+'/'+fc+'_join_ifc', tblws, fc+'_tbl_ifc.dbf')
+                        print fc+' dbf file exported at '+datetime.datetime.now().time().isoformat()[0:8]
+                    else:
+                        pass
+
+        #Merge Output DBF Tables into One File#
             arcpy.env.workspace = tblws
             dbf_list=arcpy.ListTables()
             dbf_merge=arcpy.Merge_management(dbf_list,mergeTable_dbf)
             return(dbf_merge)
             print 'script complete on '+datetime.datetime.now().date().isoformat()+' at '+datetime.datetime.now().time().isoformat()[0:8]
-                
-        else:
-            outws =  path_to_output_gdb #Output location for polygons containing points
-            # loop through features
-            for fc in fc_list:
-                print 'loading feature '+fc
-                arcpy.MakeFeatureLayer_management(polyws+'/'+fc, 'tmp_fc')
-
-                # select polygons matching field points
-                print 'selecting ecog polygons that intersect with target shapefile'
-                arcpy.SelectLayerByLocation_management('tmp_fc', "INTERSECT", 'gmu_poly', '', "NEW_SELECTION") # point intercept plots
-                # see if anything was selected
-                matchcount = int(arcpy.GetCount_management('tmp_fc')[0])
-                # if so save that fc
-                if matchcount != 0:
-                    # exporting polygons
-                    print 'exporting'
-                    arcpy.CopyFeatures_management('tmp_fc', outws+'/'+fc+'_select_gmu')
-                    print fc+'_select_gmu exported at '+datetime.datetime.now().time().isoformat()[0:8]
-                    # spatial join
-                    arcpy.MakeFeatureLayer_management(outws+'/'+fc+'_select_gmu', 'tmp_fc_sel')
-                    arcpy.SpatialJoin_analysis('tmp_fc_sel', 'gmu_poly', outws+'/'+fc+'_join_gmu', "JOIN_ONE_TO_MANY")
-                    print fc+' joined with target shapefile at '+datetime.datetime.now().time().isoformat()[0:8]
-                    # export shapefile
-                    arcpy.FeatureClassToFeatureClass_conversion(outws+'/'+fc+'_join_gmu', tblws, gmu_poly+'_intersect_'+fc+'.shp')
-                    print fc+' shapefile file exported at '+datetime.datetime.now().time().isoformat()[0:8]
-                else:
-                    pass
-
-            # Combine Output .dbfs into one table
-                arcpy.env.workspace = tblws
-                dbf_list=arcpy.ListTables()
-                dbf_merge=arcpy.Merge_management(dbf_list,mergeTable_dbf)
-                return(dbf_merge)
-                print 'script complete on '+datetime.datetime.now().date().isoformat()+' at '+datetime.datetime.now().time().isoformat()[0:8]
-            
+        
     except Exception as e:
         print '\n\n!!!---> SCRIPT CRASHED ON '+datetime.datetime.now().date().isoformat()+' AT '+datetime.datetime.now().time().isoformat()[0:8]+' <---!!!\n'
 
 # # # # # END OF FUNCTION # # # # # # #
-
